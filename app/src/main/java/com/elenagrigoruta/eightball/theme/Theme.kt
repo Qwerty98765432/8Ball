765de5b4ca18b1bf180a3dbc9e1af859d1c34c12
ElenaGrigoruta <elena.grigoruta@gmail.com>
2023-03-16 01:02:45 +0200

    now the app is full visible in light/dark mode

---
+++ app/src/main/java/com/elenagrigoruta/eightball/theme/Theme.kt
@@ -0,0 +1,53 @@
+/*
+///just in case I will try to add a button or a setting to switch from dark to light mode
+
+package com.elenagrigoruta.eightball.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+
+private val LightThemeColors = lightColors(
+    primary = Blue600,
+    primaryVariant = Blue400,
+    onPrimary = Black2,
+    secondary = Color.White,
+    secondaryVariant = Teal300,
+    onSecondary = Color.Black,
+    error = RedErrorDark,
+    onError = RedErrorLight,
+    background = Grey1,
+    onBackground = Color.Black,
+    surface = Color.White,
+    onSurface = Color.Black,
+)
+
+private val DarkThemeColors = darkColors(
+    primary = Blue700,
+    primaryVariant = Color.White,
+    onPrimary = Color.White,
+    secondary = Black1,
+    onSecondary = Color.White,
+    error = RedErrorLight,
+    background = Color.Black,
+    onBackground = Color.White,
+    surface = Black1,
+    onSurface = Color.White,
+)
+
+@Composable
+fun AppTheme(
+    darkTheme: Boolean,
+    content: @Composable () -> Unit,
+) {
+    MaterialTheme(
+        colors = if (darkTheme) DarkThemeColors else LightThemeColors,
+    ){
+        content()
+    }
+}
+
+ */
--- app/src/main/java/com/elenagrigoruta/eightball/ui/MainActivity.kt
+++ app/src/main/java/com/elenagrigoruta/eightball/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.elenagrigoruta.eightball
+package com.elenagrigoruta.eightball.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -14,6 +14,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
@@ -44,18 +45,22 @@ fun MainScreen() {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(10.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
+            horizontalArrangement = Arrangement.SpaceAround
         ) {
             Text(
                 text = "Points: $points",
-                fontSize = 18.sp,
+                fontSize = 15.sp,
                 fontWeight = FontWeight.Bold
             )
-            Button(onClick = {
+            Button(
+                onClick = {
                 isTimerRunning = !isTimerRunning
                 points = 0
             }) {
-                Text(text = if(isTimerRunning) "Reset" else "Start")
+                Text(fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    text = if(isTimerRunning) "Reset" else "Start")
             }
             CountdownTimer(
                 isTimerRunning = isTimerRunning
@@ -94,7 +99,7 @@ fun CountdownTimer(
     }
     Text(
         text = (curTime / 1000).toString(),
-        fontSize = 20.sp,
+        fontSize = 15.sp,
         fontWeight = FontWeight.Bold
     )
 }
